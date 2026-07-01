de7c61588dea47f521cd800c32dab8e5e7fbf99a
wenderroschi <lucaswecorrea@gmail.com>
2026-06-30 22:09:14 -0300

    Create Aula06.java

---
+++ aula06/Aula06.java
@@ -0,0 +1,9 @@
+package aula.pkg06;
+
+public class Aula06 {
+    public static void main(String[] args) {
+        ControleRemoto o = new ControleRemoto();
+        o.ligar();
+        o.abrirMenu();
+    }
+}
