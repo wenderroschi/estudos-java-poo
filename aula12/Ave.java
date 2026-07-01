c4b64eac7790e318d4152b8e839f8075e0cfabc7
wenderroschi <lucaswecorrea@gmail.com>
2026-06-30 22:17:43 -0300

    Create Ave.java

---
+++ aula12/Ave.java
@@ -0,0 +1,13 @@
+package aula.pkg12;
+
+public class Ave extends Animal {
+    private String corPena;
+
+    @Override public void emitirSom() { System.out.println("Som de Ave"); }
+    @Override public void alimentar() { System.out.println("Comendo frutas"); }
+    @Override public void locomover() { System.out.println("Voando"); }
+    public void fazerNinho() { System.out.println("Construiu um ninho"); }
+
+    public String getCorPena() { return corPena; }
+    public void setCorPena(String corPena) { this.corPena = corPena; }
+}
