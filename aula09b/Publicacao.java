f56caf466dec813e1a5c40e4558d06939654f4d3
wenderroschi <lucaswecorrea@gmail.com>
2026-06-30 22:10:53 -0300

    Create Publicacao.java

---
+++ aula09b/Publicacao.java
@@ -0,0 +1,9 @@
+package aula09b;
+
+public interface Publicacao {
+    public void abrir();
+    public void fechar();
+    public void folhear(int p);
+    public void avançarPag();
+    public void voltarPag(); 
+}
