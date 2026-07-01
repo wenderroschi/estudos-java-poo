198934f29d2683f0a8abbc68709a2bc00fcec327
wenderroschi <lucaswecorrea@gmail.com>
2026-06-30 22:19:25 -0300

    Create Aula12.java

---
+++ aula12/Aula12.java
@@ -0,0 +1,10 @@
+package aula.pkg12;
+
+public class Aula12 {
+    public static void main(String[] args) {
+        Canguru c = new Canguru();
+        Cachorro k = new Cachorro();
+        c.locomover();
+        k.emitirSom();
+    }
+}
