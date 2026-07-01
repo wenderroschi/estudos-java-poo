a1a3f966d2335dca7c60e3456ab38ed338ca3138
wenderroschi <lucaswecorrea@gmail.com>
2026-06-30 22:18:34 -0300

    Create Canguru.java

---
+++ aula12/Canguru.java
@@ -0,0 +1,5 @@
+package aula.pkg12;
+public class Canguru extends Mamifero {
+    public void usarBolsa() { System.out.println("Usando bolsa"); }
+    @Override public void locomover() { System.out.println("Saltando"); }
+}
