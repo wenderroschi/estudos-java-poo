86f3de9b7a1cb4330b63b72312d067ec5c20962a
wenderroschi <lucaswecorrea@gmail.com>
2026-06-30 22:14:31 -0300

    Create Bolsista.java

---
+++ aula11/Bolsista.java
@@ -0,0 +1,14 @@
+package aula11;
+
+public class Bolsista extends Aluno {
+    private float bolsa;
+    
+    public void renovarBolsa() { System.out.println("Renovando a bolsa do aluno " + this.getNome()); }
+
+    @Override public void pagarMensalidade() {
+        System.out.println(this.getNome() + " é bolsista! Pagamento facilitado.");
+    }   
+
+    public float getBolsa() { return bolsa; }
+    public void setBolsa(float bolsa) { this.bolsa = bolsa; }
+}
